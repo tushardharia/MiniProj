98954b76858c270f933f12473ae0f7d1d2cc171c
tranzzer <50516828+tranzer7@users.noreply.github.com>
2021-05-16 14:25:56 +0530

    Menu Done

---
+++ Assets/SettiingsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettiingsMenu : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+
+    public void SetVolume (float volume)
+    {
+        audiomixer.SetFloat("MusicVol", volume);
+    }
+}
